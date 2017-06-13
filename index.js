0f6e1a45612239b639480727c249b59c13634c4d
dhrubesh <dhrubesh97@gmail.com>
2017-06-13 18:48:17 +0000

    test commit (success)

--- index.js
+++ index.js
@@ -3,19 +3,33 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+//apiKey expires in 30 days
 const Bing = require('node-bing-api')({accKey : 'db7b5defd604421194f8c6e8278bb96f'});
+
+const searchTerm = require("./models/searchTerm.js");
 
 app.use(bodyParser.json());
 app.use(cors());
+mongoose.connect('mongodb://localhost/searchTerms')
 
 app.get('/api/searchimage/:searchVal*',(req,res)=>{
     var searchVal= req.params.searchVal ;
     var offset = req.query.offset ;
     
-    res.json({
+    var data = new searchTerm({
         searchVal,
-        offset
+        searchDate : new Date()
+    });
+    data.save(err=>{
+        if (err){
+            res.send("error in saving to db")
+        }
+        res.json(data)
     })
+    // res.json({
+    //     searchVal,
+    //     offset
+    // })
 })
 
 app.get('/', function (req, res) {
